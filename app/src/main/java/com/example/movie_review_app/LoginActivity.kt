56ce7a2d1cb677c011752f631347bf1b6dd46ba6
LAPTOP-6QPUH5QL\dhwns <dhwnsdyd21@gmail.com>
2023-02-14 22:51:12 +0900

    first commit

--- app/src/main/java/com/example/movie_review_app/LoginActivity.kt
+++ app/src/main/java/com/example/movie_review_app/LoginActivity.kt
@@ -2,12 +2,23 @@ package com.example.movie_review_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movie_review_app.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        val view = binding.root
+
+        setContentView(view)
+
+
     }
+
+
 }
