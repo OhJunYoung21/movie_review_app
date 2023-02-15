20a5e484921539ec4f035d85be7aa3259fc9d36b
LAPTOP-6QPUH5QL\dhwns <dhwnsdyd21@gmail.com>
2023-02-15 17:31:59 +0900

    commit

--- app/src/main/java/com/example/movie_review_app/LoginActivity.kt
+++ app/src/main/java/com/example/movie_review_app/LoginActivity.kt
@@ -1,15 +1,18 @@
 package com.example.movie_review_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.movie_review_app.databinding.ActivityLoginBinding
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
 
     lateinit var mAuth: FirebaseAuth
 
@@ -20,10 +23,37 @@ class LoginActivity : AppCompatActivity() {
 
         setContentView(view)
 
+        binding.Signin.setOnClickListener {
 
+            val email = binding.email.text.toString().trim()
+            val password = binding.password.text.toString().trim()
+
+
+            login(email, password)
+
+        }
+        binding.Signup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
 
+
+    private fun login(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            } else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
 
 }
--- app/src/main/java/com/example/movie_review_app/SignupActivity.kt
+++ app/src/main/java/com/example/movie_review_app/SignupActivity.kt
@@ -1,11 +1,34 @@
 package com.example.movie_review_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movie_review_app.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+
+    lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+
+        setContentView(binding.root)
+
+        binding.complete.setOnClickListener {
+
+            val name = binding.name.text.toString().trim()
+            val email = binding.email.text.toString().trim()
+            val password = binding.passwordSignup.text.toString().trim()
+
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
     }
+
+
 }
